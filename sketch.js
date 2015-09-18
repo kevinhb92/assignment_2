fe34eddfbe9b3d90ee7de0fa1e7f967a171e3581
kevinhb92 <leeh965@Kevinui-MacBook-Pro.local>
2015-09-18 02:58:16 -0400

    gave a randomness

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
 
 function draw() {
 	// clear the background
-	background(16,55,82);
+	background(189,189,189);
 
 	// set a fill color for stage
 	fill(89, 63, 63);
@@ -15,6 +15,24 @@ function draw() {
 	noStroke();
 	rect(0, 300, 640, 100);
 
+  //draw window
+	fill(199,251,253)
+	rect(173, 25, 110,70)
+
+	//draw sky
+
+	fill(255,255,0)
+  ellipse(203,40,20,20)
+
+	fill(255,255,255)
+	ellipse(232,60,30,20)
+	ellipse(242,70,30,20)
+	ellipse(252,70,30,20)
+
+  //draw wall
+	fill(139,138,136)
+  rect(0, 0, 140, 290)
+  rect(490, 0, 160, 290)
   // set a fill color for ground
   fill(52,2,21)
 
@@ -30,7 +48,7 @@ function draw() {
 	rect(0, 230, 640, 10)
 	rect(0, 215, 640, 10)
 	rect(0, 200, 640, 10)
-  fill(16, 55, 82);
+  fill(139,138,136);
 	triangle(0,300,150,200,0,200)
 	triangle(640,300,640,200,490,200)
 
@@ -86,12 +104,12 @@ function draw() {
 	ellipse(160, 120, 32, 65)
 	ellipse(122, 140, 35, 35);
 	ellipse(178, 140, 35, 35)
-	arc(150, 135, 70, 70, 0, PI);
+	arc(150, 135, 80, 70, 0, PI);
 
   //draw eyes for character
 	fill(255,255,255)
-	ellipse(140, 115, 16, 30)
-	ellipse(160, 115, 16, 30)
+	ellipse(140, 115, 18, 30)
+	ellipse(160, 115, 18, 30)
 	ellipse(150, 135, 6, 2)
 
 	//draw nose for character
@@ -102,13 +120,79 @@ function draw() {
 	fill(255,255,255)
 	ellipse(152, 133, 13, 3)
 
-	//draw mouth for character
-	fill(57,0,11)
-	arc(150, 145, 60, 30, 0, PI);
-	fill(163,90,83)
-	arc(150, 155, 20, 10, 0, PI);
 
  //draw a stik for the character
+ fill(214,165,88)
+ rect(125, 215, 89,4)
+ rect(98, 215, 10,4)
+
+  //draw a chalk board
+	fill(214,165,88)
+  rect(475, 215, 15,40)
+  rect(275, 215, 15,40)
+	 rect(253, 255, 260,10)
+   fill(89,54,0)
+	 rect(243, 205, 280,10)
+
+
+  fill(161,141,101)
+  rect(253, 25, 260,180)
+
+		fill(87,86,84)
+		rect(263, 35, 240,160)
+
+ //chalk board eraser
+
+	fill(181,181,181)
+	rect(293, 190, 50,20,20)
+	fill(0,0,0)
+ 	rect(293, 190, 50,5,20)
+
+
+//chalk
+	fill(255,255,255)
+	rect(383, 200, 20,5,20)
+	rect(433, 200, 30,5,20)
+
+
+
+	var dayNight = random(1);
+
+if (dayNight < 0.5) {
+			 fill(0,0,0);
+			 ellipse(165,105,10,10);
+       ellipse(145,105,10,10);
+		 } else {
+			 fill(0,0,0)
+			 ellipse(165,122,10,10)
+       ellipse(145,122,10,10)
+		}
+
+
+if (dayNight < 0.5) {
+		//draw mouth for character
+		fill(57,0,11)
+		arc(150, 145, 60, 30, 0, PI);
+		fill(163,90,83)
+		arc(150, 155, 20, 10, 0, PI);
+	} else {
+		fill(157,0,0)
+		ellipse(155,162,10,10)
+
+	}
+if (dayNight < 0.5) {
+	//draw light bulb
+  fill(255,255,255)
+  ellipse(80,50,50,50)
+  rect(70,50,20,40,20)
+  rect(70,50,20,55,20)
+  fill(217,217,217)
+  rect(69,80,22,20)
+  fill(255,255,0)
+  ellipse(80,50,30,30)
+}
+
+
 
 
 }
